739b38695997446fe5dc7adaccbc4741a2864911
eva <83175995+evapopp@users.noreply.github.com>
2021-10-12 15:06:09 -0700

    added get route to workouts/range

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -13,6 +13,17 @@ router.get('/api/workouts', async (req, res) => {
     res.status(200).json(workoutData);
 })
  
+router.get('/api/workouts/range', async (req, res) => {
+    const workoutDataRange = await Workout.aggregate([
+        {
+            $addFields: {
+                duration: { $sum: '$exercise.duration' }
+            }
+        }
+    ])
+    res.status(200).json(workoutDataRange);
+})
+
 router.post('/api/workouts', async ({body}, res) => {
     const workoutCreate = await Workout.create(body)
     res.status(200).json(workoutCreate);
